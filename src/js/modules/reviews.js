310176c62f47af82846d75baf2c8dae20347c753
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-04-29 22:54:44 +0500

    reviews блок

--- src/js/modules/reviews.js
+++ src/js/modules/reviews.js
@@ -1,10 +1,27 @@
-import Swiper, {Pagination, Navigation}  from 'swiper';
+import Swiper, { Pagination, Navigation, Keyboard } from 'swiper';
 
 import 'swiper/css';
 
 function initSlider() {
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper('.reviews__swiper', {
+        modules: [Pagination, Navigation, Keyboard],
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            bulletClass: "reviews__bullet",
+            bulletActiveClass: "reviews__bullet--current",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".reviews__arrow--next",
+            prevEl: ".reviews__arrow--prev",
 
+        },
+        autoHeight: true,
+        loop: true,
     });
 }
 
