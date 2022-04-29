4351400e98bda44d03068d25c7a74ed3cd32a327
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-04-29 19:38:00 +0500

    temp

---
+++ src/js/modules/reviews.js
@@ -0,0 +1,14 @@
+import Swiper, {Pagination, Navigation}  from 'swiper';
+
+import 'swiper/css';
+
+function initSlider() {
+    const swiper = new Swiper('.swiper', {
+
+    });
+}
+
+
+
+
+export default initSlider;
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 import menu from './modules/menu.js'
+import reviews from './modules/reviews.js'
 
 
 document.querySelector('.no-js').classList.remove('no-js');
 
 menu();
+reviews();
